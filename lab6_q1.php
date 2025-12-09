abc2e0aa0b280f98f1310023049b8fe1cd6c1830
naqibrazak <naqbrzk@gmail.com>
2025-12-09 15:32:23 +0800

    Initial lab_6 Webdev

---
+++ lab6_q1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 6 Q1</title>
+</head>
+
+<body>
+    <?php
+        $name = "Naqib Razak";
+        $matric = "AI230071";
+        $course = "BIP Bachelor of Computer Science (Software Engineering)";
+        $year = "Year 3";
+        $address = "Universiti Tun Hussein Onn Malaysia (UTHM), 86400 Parit Raja, Batu Pahat, Johor, Malaysia"
+    ?>
+
+    <h2>My Details</h2>
+
+    <table>
+        <tr>
+            <td>Name</td>
+            <td><?php echo $name; ?></td>
+        </tr>
+
+
+</html>
